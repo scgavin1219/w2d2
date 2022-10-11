52727d2627f78cb84a924539d7686252c330e6fe
Dylan Gavin <dyl.gavin@gmail.com>
2022-10-10 18:49:07 -0700

    class work with startup project

--- dylan_gavin_startup_project/lib/employee.rb
+++ dylan_gavin_startup_project/lib/employee.rb
@@ -1,3 +1,15 @@
 class Employee
+    attr_reader :name, :title
+    attr_writer :earnings
 
+    def initialize(name, title)
+        @name = name
+        @title = title
+        @earnings = 0 
+    end
+
+    def pay(amount)
+        @earnings += amount
+    end
+    
 end
--- dylan_gavin_startup_project/lib/startup.rb
+++ dylan_gavin_startup_project/lib/startup.rb
@@ -1,5 +1,68 @@
 require "employee"
 
 class Startup
+    attr_reader :name, :funding, :salaries, :employees
 
+    def initialize(name, funding, salaries)
+        @name = name
+        @funding = funding
+        @salaries = salaries
+        @employees = []
+    end
+
+    def valid_title?(title)
+        @salaries.keys.include?(title)
+    end
+
+    def >(other_startup)
+        self.funding > other_startup.funding
+    end
+
+    def hire(employee_name, title)
+        if @salaries.keys.include?(title)
+            @employees << Employee.new(employee_name, title)
+        else
+            raise "error"
+        end
+    end
+
+    def size
+        @employees.length
+    end
+
+    def pay_employee(employee)
+        salary = @salaries[employee.title]
+        if salary <= @funding
+            employee.pay(salary)
+            @funding -= salary
+        else
+            raise "error"
+        end
+    end
+
+    def payday
+        @employees.each { |employee| pay_employee(employee) }
+    end
+
+    def average_salary
+        sum = 0 
+        @employees.each { |employee| sum += @salaries[employee.title]}
+        sum / @employees.length
+    end
+
+    def close
+        @employees = []
+        @funding = 0
+    end
+
+    def acquire(other_startup)
+        @funding += other_startup.funding
+        other_startup.salaries.each do |title, salary|
+            if !@salaries.has_key?(title)
+                @salaries[title] = salary
+            end
+        end
+        @employees += other_startup.employees
+        other_startup.close
+    end
 end
